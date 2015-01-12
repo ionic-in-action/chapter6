e8310dcd0ae7a774bb6e8e73ae9fd3bb32a438f7
Jeremy Wilken <gnomation@gnomeontherun.com>
2015-01-11 23:38:59 -0600

    add weather forecast content and styling with filters

--- www/js/app.js
+++ www/js/app.js
@@ -38,6 +38,44 @@ angular.module('App', ['ionic'])
   $scope.locations = Locations.data;
 })
 
+.filter('timezone', function () {
+  return function (input, timezone) {
+    if (input && timezone) {
+      var time = moment.tz(input * 1000, timezone);
+      return time.format('LT');
+    }
+    return '';
+  };
+})
+
+.filter('chance', function () {
+  return function (chance) {
+    if (chance) {
+      var value = Math.round(chance * 10);
+      return value * 10;
+    }
+    return 0;
+  };
+})
+
+.filter('icons', function () {
+  var map = {
+    'clear-day': 'ion-ios-sunny',
+    'clear-night': 'ion-ios-moon',
+    rain: 'ion-ios-rainy',
+    snow: 'ion-ios-snowy',
+    sleet: 'ion-ios-rainy',
+    wind: 'ion-ios-flag',
+    fog: 'ion-ios-cloud',
+    cloudy: 'ion-ios-cloudy',
+    'partly-cloudy-day': 'ion-ios-partlysunny',
+    'partly-cloudy-night': 'ion-ios-cloudy-night'
+  };
+  return function (icon) {
+    return map[icon] || '';
+  }
+})
+
 .factory('Settings', function () {
   var Settings = {
     units: 'us',
--- www/views/weather/weather.js
+++ www/views/weather/weather.js
@@ -6,4 +6,15 @@ angular.module('App')
   $http.get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.lng, {params: {units: Settings.units}}).success(function (forecast) {
     $scope.forecast = forecast;
   });
+
+  var barHeight = document.getElementsByTagName('ion-header-bar')[0].clientHeight;
+  $scope.getWidth = function () {
+    return window.innerWidth + 'px';
+  };
+  $scope.getTotalHeight = function () {
+    return parseInt(parseInt($scope.getHeight()) * 3) + 'px';
+  };
+  $scope.getHeight = function () {
+    return parseInt(window.innerHeight - barHeight) + 'px';
+  };
 });
